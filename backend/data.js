1c658d582d31b8d22ced67b0eb154e5c2f1a6e6b
TANUSMITA GHOSH <tanusmitanew@gmail.com>
2020-08-07 14:26:04 +0530

    first commit

---
+++ backend/data.js
@@ -0,0 +1,37 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 600,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 6,
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 500,
+            brand: 'Nike',
+            rating: 4.1,
+            numReviews: 10
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 450,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 3,
+            countInStock: 6,
+        },
+    ]
+
+}
